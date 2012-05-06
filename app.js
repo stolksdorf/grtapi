3feafb60c9536a02eb6b1866ceda0c8b552cb4d4
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-05-06 00:00:21 -0400

    K

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80, '107.21.214.193');
-console.log('Server running at http://107.21.214.193:80/');
+var app = require('express').createServer();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(80);
