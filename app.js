fa4e9e750d645049846a3bf9b437f4eda6553324
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-05-05 23:39:15 -0400

    k

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(80, '107.21.214.193');
+console.log('Server running at http://107.21.214.193:80/');
