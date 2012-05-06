1d2d6b6ceff5a4fa45f7182f3b3117a67432da8d
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-05-06 15:55:33 -0400

    loc

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 var application_root = __dirname,
-    express = require("express");
+    express = require("express"),
     path = require("path"),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+
 
 var app = express.createServer();
 
-// Database
-
-mongoose.connect('mongodb://localhost/grtapiDB');
-
-//Schema Design
-
-var Schema = mongoose.Schema;
-var Widget = new Schema({
-	key      : {type : String},
-	value    : {type : String},
-	modified : {type : Date, default : Date.now}
-});
-
-
-var WidgetModel = mongoose.model('Widget', Widget);
-
-
-
 // Config
-
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -33,15 +15,166 @@ app.configure(function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+// Database
+mongoose.connect('mongodb://localhost/grtapiDB');
 
-//REST Commands
+//Schema Design
+var Schema = mongoose.Schema;
+
+var StopSchema = new Schema({
+	stopnum  : {type : String},
+	gpslon   : {type : Number},
+	gpslat   : {type : Number},
+	stopname : {type : String}
+});
+
+var BusSchema = new Schema({
+	stopnum  : {type : String},
+	busnum   : {type : String},
+	busdesc  : {type : String}
+});
+
+var TimeSchema = new Schema({
+	stopnum  : {type : String},
+	busnum   : {type : String},
+	time     : {type : String}
+});
+
+var StopModel = mongoose.model('Stop', StopSchema);
+var BusModel  = mongoose.model('Bus' , BusSchema);
+var TimeModel = mongoose.model('Time', TimeSchema);
+
+/*
+var Widget = new Schema({
+	key      : {type : String},
+	value    : {type : String},
+	modified : {type : Date, default : Date.now}
+});
+var WidgetModel = mongoose.model('Widget', Widget);
+*/
+
+
+
+
+
+
+
+
+//Launch Site
 app.get('/api', function (req, res) {
   res.send('GRTAPI is running, fuck yeah!');
 });
 
+
+
+///////////REST GET Commands
+app.get('/api/stop', function(req, res){
+	StopModel.find({}, function(error, stops){
+		if(!error){
+			return res.send(stops);
+		}else{
+			return console.log("GET Stop ERR: ", error);
+		}
+	});
+});
+
+app.get('/api/bus', function(req, res){
+	BusModel.find({}, function(error, buses){
+		if(!error){
+			return res.send(buses);
+		}else{
+			return console.log("GET Bus ERR: ", error);
+		}
+	});
+});
+
+app.get('/api/time', function(req, res){
+	TimeModel.find({}, function(error, times){
+		if(!error){
+			return res.send(times);
+		}else{
+			return console.log("GET Time ERR: ", error);
+		}
+	});
+});
+
+app.get('/api/search', function(req, res){
+
+	return res.send('Implementating this later bro');
+
+});
+
+
+
+
+
+
+
+
+////////////REST POST Commands
+app.post('/api/stop', function(req, res){
+	var stop = new StopModel({
+		stopnum  : req.body.stopnum,
+		gpslon   : req.body.gpslon,
+		gpslat   : req.body.gpslat,
+		stopname : req.body.stopname
+	});
+
+	stop.save(function (err) {
+		if (!err) {
+			return console.log("Added New Stop: " + req.body.stopnum);
+		} else {
+			return console.log("POST Stop ERR: ", err);
+		}
+	});
+	return res.send(stop);
+});
+
+app.post('/api/bus', function(req, res){
+	var bus = new BusModel({
+		stopnum  : req.body.stopnum,
+		busnum   : req.body.busnum,
+		busdesc  : req.body.busdesc
+	});
+
+	bus.save(function (err) {
+		if (!err) {
+			return console.log("Added New Bus: " + req.body.busnum);
+		} else {
+			return console.log("POST Bus ERR: ", err);
+		}
+	});
+	return res.send(bus);
+});
+
+app.post('/api/time', function(req, res){
+	var time = new TimeModel({
+		stopnum  : req.body.stopnum,
+		busnum   : req.body.busnum,
+		time     : req.body.busdesc
+	});
+
+	time.save(function (err) {
+		if (!err) {
+			return console.log("Added New Time: " + req.body.stopnum + 
+			                   " for " + req.body.busnum + 
+			                   " at " + req.body.time);
+		} else {
+			return console.log("POST Time ERR: ", err);
+		}
+	});
+	return res.send(time);
+});
+
+
+
+
+
+/*
+
 app.get('/api/widget' , function(req, res){
 	console.log("Received a GET Request", req.body);
-	return WidgetModel.find({}, function (err, widgets){
+	WidgetModel.find({}, function (err, widgets){
 		console.log("widgets:", widgets);
 		if(!err){
 			return res.send(widgets);
@@ -69,7 +202,9 @@ app.post('/api/widget', function(req, res){
 	});
 
 	return res.send(widget);
-})
+});
+
+*/
 
 
 
@@ -78,8 +213,5 @@ app.post('/api/widget', function(req, res){
 
 
 
-
-
-// Launch server
-
+//////////// Launch server
 app.listen(80);
--- grt data/conversion.php
+++ grt data/conversion.php
@@ -1,18 +1,173 @@
 <?php 
+include_once(dirname(__FILE__).  '\pest\PestJSON.php');
+
+$baseUrl = 'http://107.21.214.193/api';
+
+//Database vars
+$inTimes = array();
+$inStops = array();
+$inTrips = array();
+
+$outStops = array();
+$outBuses = array();
+$outTimes = array();
 
 
+function extractCSV($path){	
+	//$basePath = '\GRT_GTFS';
+	$basePath = '\testGTFS';
 
-function extractCSV($path){
-	$tokens = explode("\n", file_get_contents(dirname(__FILE__) . $path));
-	$index = explode(",", $tokens[0]);
+	$tokens = explode("\n", file_get_contents(dirname(__FILE__) . $basePath . $path));
+	$index = explode(",", str_replace(array("\r", "\n"), '', $tokens[0]));
 	$result = array();
 	for($t=1; $t < count($tokens); $t++){
 		if($tokens[$t] != ""){
+			$tokens[$t] = str_replace(array("\r", "\n"), '', $tokens[$t]);
 			array_push($result, array_combine($index, explode(",", $tokens[$t])));
 		}
 	} 
 	return $result;
 }
+
+
+
+
+
+function extractRawFiles(){
+	global $inTimes, $inStops, $inTrips;
+	echo "Extracting Stops\n";
+	$inStops = extractCSV('\stops.txt');
+
+	echo "Extracting Trips\n";
+	$inTrips = extractCSV('\trips.txt');
+
+	echo "Extracting Stop Times\n";
+	$inTimes = extractCSV('\stop_times.txt');
+
+	echo "Finished Extraction.\n";
+}
+
+function printDatabaseVars(){
+	global $outStops, $outBuses, $outTimes;
+	echo print_r($outStops, true);
+	echo print_r($outTimes, true);
+	echo print_r($outBuses, true);
+}
+
+
+
+
+
+function buildStopTable(){
+	global $inStops, $outStops;
+	foreach($inStops as $stop){
+		array_push($outStops, array(
+				'stopnum' => $stop['stop_id'],
+				'address' => $stop['stop_name'],
+				'gpslat'  => $stop['stop_lat'],
+				'gpslon'  => $stop['stop_lon']
+		));
+	}
+}
+
+
+
+function buildBusAndTimeTable(){
+	global $inTimes, $inTrips, $outBuses, $outTimes;
+
+	$processedBuses = array();
+	foreach($inTimes as $time){
+		$trip = findInTrip($time['trip_id']);
+
+		//process the bus num
+		$busnum = $trip['route_id'];
+		$busdesc = $trip['trip_headsign'];		
+		if($trip['trip_headsign'][1] == ' '){
+			$busnum = $busnum . $trip['trip_headsign'][0];
+			$busdesc = substr($busdesc,2);
+		}
+		array_push($outTimes, array(
+		           'stopnum' => $time['stop_id'],
+		           'busnum'  => $busnum,
+		           'time'    => $time['departure_time']
+		));
+
+		if(!in_array($busnum, $processedBuses)){
+			array_push($outBuses, array(
+			        'stopnum' => $time['stop_id'],
+			        'busnum'  => $busnum,
+			        'busdesc' => $busdesc
+			));
+			array_push($processedBuses, $busnum);
+		}
+	}
+}
+
+
+
+//Returns a trip obj based on a trip ID
+function findInTrip($tripId){
+	global $inTrips;
+	foreach($inTrips as $trip){	
+		if($tripId == $trip['trip_id']){
+			return $trip;
+		}	
+	}
+	return array();
+}
+
+
+
+
+
+extractRawFiles();
+buildStopTable();
+buildBusAndTimeTable();
+printDatabaseVars();
+
+
+
+//echo print_r($outStops,true);
+
+//echo print_r(findInTrip("549305"));
+
+
+echo "Finished";
+
+
+
+
+
+
+/*Steps
+
+1) extract out the 3 needed csv
+2) build the stops files super easy
+3) Loop through the inStopTimes
+*/
+
+/*
+listOfProcessedBuses = array();
+
+foreach(timeRow in stopTimes){
+	tripRow = getTrip(timeRow.trip_id);
+	outTimes.push(
+		'stopnum' => timeRow.stop_id,
+		'busnum'  => tripRow.route_id, //requires some post-processing
+		'time'    => timeRow.depart_time
+	);
+	if(listOfProcessedBuses does not contain tripRow.route_id){
+		outBuses.push(
+			'stopnum' => timeRow.stop_id,
+			'busnum'  => tripRow.route_id,
+			'busdesc' => tripRow.trip_headsign		
+		);
+		add to list of processed buses
+	}
+}
+
+
+
 
 
 //echo print_r(extractCSV('/GRT_GTFS/agency.txt'), true);
@@ -21,4 +176,44 @@ function extractCSV($path){
 //extractCSV('/GRT_GTFS/stops.txt');
 //extractCSV('/GRT_GTFS/trips.txt');
 //extractCSV('/SampleGTFS/stop_times.txt');
-echo "Finished";
+
+
+
+$pest = new pestJSON($baseUrl);
+
+/*
+try{
+	$widget = $pest->get('/widget');
+	echo print_r($widget,true);
+} catch(Pest_UnknownResponse $e){
+	echo print_r($e,true);
+}
+
+
+*/
+
+/*
+echo print_r($pest->post('/widget', 
+	array(
+		'key' => 'foo3',
+		'value' => 'Yeah yeah!'
+	)
+));
+
+
+/*
+
+
+
+   
+        $baseUrl = $this->getApiBaseUrl();
+        $pest = new PestJSON($baseUrl);
+    
+        $pest->setupAuth($this->apiKey, $this->apiSecret);
+        $this->_restClient = $pest;
+    
+        return $this->_restClient;
+    }
+    
+    
+*/
