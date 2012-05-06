e73c33eabeefe3b03fd500253f1583d5984fc890
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-05-06 03:22:40 -0400

    Trying to get the structure going

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
+/*
 var app = require('express').createServer();
 
 app.get('/', function(req, res){
   res.send('hello my sweetest banana');
 });
+
+app.listen(80);
+*/
+
+///////////////////////////
+
+var application_root = __dirname,
+    express = require("express");
+   // path = require("path"),
+    //mongoose = require('mongoose');
+
+var app = express.createServer();
+
+// Database
+
+//mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/api', function (req, res) {
+  res.send('Ecomm API is running');
+});
+
+// Launch server
 
 app.listen(80);
