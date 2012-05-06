e78e7f1b547f24630f014c90b15213be7b13a276
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-05-06 05:43:11 -0400

    Added a get and post for 'widgets', just a dummy value

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express.createServer();
 
 // Database
 
-//mongoose.connect('mongodb://localhost/ecomm_database');
+mongoose.connect('mongodb://localhost/grtapiDB');
 
 // Config
 
@@ -19,9 +19,61 @@ app.configure(function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+
+//REST Commands
 app.get('/api', function (req, res) {
   res.send('Ecomm API is running');
 });
+
+app.get('/api/widget' , function(req, res){
+	return WidgetModel.find(function (err, widgets){
+		if(!err){
+			return res.send(widgets);
+		} else {
+			return console.log(err);
+		}
+	});
+});
+
+app.post('/api/widget', function(req, res){
+	var widget;
+	console.log("POST: ");
+	console.log(req.body);
+
+	widget = new ProductModel({
+		key  : req.body.key,
+		value: req.body.value,
+	});
+
+	widget.save(function (err) {
+		if (!err) {
+			return console.log("created");
+		} else {
+			return console.log(err);
+		}
+	});
+
+	return res.send(widget);
+})
+
+
+//Schema Design
+
+var Schema = mongoose.Schema;
+var Widget = new Schema({
+	key      : {type : String, required : true}.
+	value    : {type : String},
+	modified : {type : Date, default : Date.now}
+});
+
+
+var WidgetModel = mongoose.model('Widget', Widget);
+
+
+
+
+
+
 
 // Launch server
 
